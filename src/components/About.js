cea5cda2c84656eceadee232f4e9e7275a2dcc47
Gabriel Viox <gviox17@gmail.com>
2021-09-21 16:58:00 -0500

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,18 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    About
+    <h2>
+      About Me
+    </h2>
+    <p>
+    What you know about me? Watchchu, Whatchu know about me?
+    </p>
+    <img src={image} alt="I made this">
+
+    </img>
+    </div>;
 }
 
 export default About;
